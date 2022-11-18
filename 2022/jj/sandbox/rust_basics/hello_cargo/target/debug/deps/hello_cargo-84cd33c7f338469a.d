da8f727f0b7f2739962834b49d17d10a2fb06115
JohnJacobsonIII <jrjacobsoniii@gmail.com>
2022-11-18 07:29:28 -0700

    jj cleanup, set own rust gitignore

--- 2022/jj/sandbox/rust_basics/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d
+++
@@ -1,5 +0,0 @@
-/home/jjacoiii/repos/AdventOfCode/2022/jj/sandbox/rust_basics/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a: src/main.rs
-
-/home/jjacoiii/repos/AdventOfCode/2022/jj/sandbox/rust_basics/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d: src/main.rs
-
-src/main.rs:
--- 2022/jj/sandbox/rust_basics/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/jjacoiii/repos/AdventOfCode/2022/jj/sandbox/rust_basics/hello_cargo/target/debug/hello_cargo: /home/jjacoiii/repos/AdventOfCode/2022/jj/sandbox/rust_basics/hello_cargo/src/main.rs
