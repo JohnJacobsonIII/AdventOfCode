98c042d5c80ec85e5f0a742a442738c5ec9a1a6d
JohnJacobsonIII <jrjacobsoniii@gmail.com>
2022-12-01 09:24:33 -0700

    jj day1 first solution

--- 2022/jj/day1/Main.hs
+++ 2022/jj/day1/Main.hs
@@ -1,4 +1,9 @@
 module Main where
 
+import Data.List
+
+adventFunc :: [[Int]] -> [Int]
+adventFunc xs = sort $ map (\ys -> foldl (+) 0 ys) (filter (not . null) xs)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = interact $ show . adventFunc . map (map read) . scanl (\acc x -> if x == "" then [] else x:acc) [] . lines 
