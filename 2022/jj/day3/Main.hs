e2d956ce5eda1b13fb427b16f443cdf77b826dec
JohnJacobsonIII <jrjacobsoniii@gmail.com>
2022-12-03 14:23:04 -0700

    day3, part1

--- 2022/jj/day3/Main.hs
+++ 2022/jj/day3/Main.hs
@@ -1,4 +1,32 @@
 module Main where
 
+import Data.List
+
+testInput = unlines ["vJrwpWtwJgWrhcsFMMfFFhFp",
+                     "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                     "PmmdzqPrVvPwwTWBwg",
+                     "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                     "ttgJtRGJQctTZtZT",
+                     "CrZsJsPPZsGzwwsLwLmpwMDw"]
+
+processInput :: String -> [String]
+processInput = lines
+
+halfStringLength :: String -> Int
+halfStringLength = (\x -> div x 2) . length
+
+findMatchingChar :: String -> String
+findMatchingChar xs = let hlen = halfStringLength xs in intersect (take hlen xs) (drop hlen xs)
+
+-- values are a=1 .. z=26, A=27 .. Z=52, and 'a'=97, 'A'=65
+priorityConvert :: String -> Int
+priorityConvert = (\xs -> let c = fromEnum $ xs!!0 in if c>96 then c-96 else c-38)
+
+totalPriority :: [Int] -> Int
+totalPriority = foldl (+) 0
+
+adventFunc :: [String] -> Int
+adventFunc = totalPriority . map priorityConvert . map findMatchingChar
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = interact $ show . adventFunc . processInput
